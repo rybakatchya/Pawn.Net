e120bba0ce4f51c573e456ba0b16bff848456dbf
russDevAcc <43803371+russDevAcc@users.noreply.github.com>
2022-06-28 20:22:40 -0400

    Update NativeLibrary.cs

--- Pawn.Net/PawnBindings/NativeLibrary.cs
+++ Pawn.Net/PawnBindings/NativeLibrary.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 
 public class NativeLibrary
 {
+#if WINDOWS
 	private const string Kernel32 = "Kernel32";
 	[DllImport(Kernel32, EntryPoint = "LoadLibraryW", SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true, BestFitMapping = false, CallingConvention = CallingConvention.Winapi)]
 	private static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPWStr)] string name);
@@ -13,27 +14,108 @@ public class NativeLibrary
 
 	[DllImport(Kernel32, EntryPoint = "FreeLibrary", SetLastError = true, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
 	private static extern bool FreeLibrary(IntPtr module);
+	
+#elif LINUX
+
+	private const string LibDl = "libdl";
+	private const UnmanagedType StringType =
+			UnmanagedType.LPStr;
+
+	[Flags]
+	private enum LibDlFlags
+	{
+		// ReSharper disable UnusedMember.Local
+		Lazy = 0x00001,
+		Now = 0x00002,
+		Global = 0x00100,
+		Local = 0,
+		NoDelete = 0x01000,
+		NoLoad = 0x00004,
+		DeepBind = 0x00008
+		// ReSharper restore UnusedMember.Local
+	}
+
+
+	[DllImport(LibDl, EntryPoint = "dlopen", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr DlOpen([MarshalAs(StringType)] string name, LibDlFlags flags);
+
+	[DllImport(LibDl, EntryPoint = "dlsym", CharSet = CharSet.Ansi, ExactSpelling = true, BestFitMapping = false, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr DlSym(IntPtr module, [MarshalAs(StringType)] string name);
+
+	[DllImport(LibDl, EntryPoint = "dlclose", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int DlClose(IntPtr handle);
+
+	[DllImport(LibDl, EntryPoint = "dlerror", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr DlError();
+#endif
+
 	public static bool TryLoad(string name, out IntPtr handle)
 	{
+		try
+		{ 
+#if WINDOWS
 		handle = LoadLibrary(name);
+#elif LINUX
+
+			handle = DlOpen(null, LibDlFlags.Local | LibDlFlags.Now);
+		}
+		catch
+		{
+			handle = IntPtr.Zero;
+			return false;
+		}
+#endif
 		return handle != IntPtr.Zero;
 	}
 
 	public static IntPtr Load(string name)
 	{
-		return LoadLibrary(name);
+		IntPtr handle = IntPtr.Zero;
+
+#if WINDOWS
+		handle = LoadLibrary(name);
+#elif LINUX
+		handle = DlOpen(null, LibDlFlags.Local | LibDlFlags.Now);
+#endif
+		return handle;
+
 	}
 
 	public static IntPtr GetExport(IntPtr handle, string name)
 	{
-		return GetProcAddress(handle, name);
+		IntPtr symbol = IntPtr.Zero;
+#if WINDOWS
+		symbol = GetProcAddress(handle, name);
+#elif LINUX
+
+		if (name == null)
+			throw new ArgumentNullException(nameof(name));
+		if (string.IsNullOrWhiteSpace(name) || name.Trim() == "\0")
+			throw new ArgumentException("Empty or whitespace symbol names are not allowed", nameof(name));
+		symbol = DlSym(handle, name);
+#endif
+		return symbol;
 	}
 
 	public static bool TryGetExport(IntPtr handle, string name, out IntPtr address)
 	{
-		var val = GetProcAddress(handle, name);
-		address = val;
-		return val != IntPtr.Zero;
+		IntPtr symbol = IntPtr.Zero;
+        try
+        {
+#if WINDOWS
+			symbol = GetProcAddress(handle, name);
+#elif LINUX
+            symbol = DlSym(handle, name);
+#endif
+		}
+        catch
+        {
+			address = IntPtr.Zero;
+			return false;
+        }
+
+		address = symbol;
+		return symbol != IntPtr.Zero;
 	}
 
 	 
